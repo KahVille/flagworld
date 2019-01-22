566f3f3f8e2ef000e7d365ec2e823d4529945ce9
Ville Kähärä <kahara.ville@gmail.com>
2019-01-22 10:47:19 +0200

    added localization manager
    added localization manager based on the offical unity tutorial. Localized texts are located in the streaming assests folder

---
+++ FlagWorldGame/Assets/SharedAssets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class LocalizationManager : MonoBehaviour {
+
+    public static LocalizationManager Instance;
+
+    private Dictionary<string, string> localizedText;
+
+    private bool isReady = false;
+    private string missingTextString = "Localized Text not Found";
+	// Use this for initialization
+	void Awake () {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+	}
+
+    public void loadLocalizedText(string fileName = null)
+    {
+        localizedText = new Dictionary<string, string>();
+        string filePath = Path.Combine(Application.streamingAssetsPath,fileName);
+        if(File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+
+            for (int i = 0; i < loadedData.items.Length; i++)
+            {
+                localizedText.Add(loadedData.items[i].key, loadedData.items[i].value);
+            }
+
+            Debug.Log("Data loaded, Dictionary contains: " + localizedText.Count + " entries");
+        }
+        else
+        {
+            Debug.LogError("Canot find file");
+        }
+
+        isReady = true;
+
+    }
+
+    public string getLocalizedValue(string key)
+    {
+        string result = missingTextString;
+        if(localizedText.ContainsKey(key))
+        {
+            result = localizedText[key];
+        }
+
+        return result;
+
+    }
+
+    public bool getIsReady()
+    {
+        return isReady;
+    }
+
+}
---
+++ FlagWorldGame/Assets/SharedAssets/Scripts/Localization/LocalizedText.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LocalizedText : MonoBehaviour, ILocalizedText {
+
+    [SerializeField]
+    private string key;
+
+	// Use this for initialization
+	void Start () {
+        Text text = GetComponent<Text>();
+        text.text = LocalizationManager.Instance.getLocalizedValue(key);
+	}
+
+    public void GetLocalizedText()
+    {
+        Text text = GetComponent<Text>();
+        text.text = LocalizationManager.Instance.getLocalizedValue(key);
+    }
+
+    /*
+     *
+     *   List<GameObject> LocalizedObjects
+         for (int i = 0; i < localizedObjects.Count; i++)
+            {
+                localizedObjects[i].GetComponent<ILocalizedText>().GetLocalizedText();
+            }
+     * 
+     */
+}
