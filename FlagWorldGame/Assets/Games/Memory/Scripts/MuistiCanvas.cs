22a5ea156e591e0a6f40343c143d027b1888b6af
Helistinify <Heikki.vayrynen92@gmail.com>
2019-01-22 14:31:50 +0200

    MemoryWorking
    Memory game works, canvases need more info.

---
+++ FlagWorldGame/Assets/Games/Memory/Scripts/MuistiCanvas.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MuistiCanvas : MonoBehaviour {
+
+    private Canvas StartC, HUD, End;
+    private string LanguageID = "Language";
+    private string CurrentGameHigh = "CurrGameHigh";
+    private int LID, points;
+
+	// Use this for initialization
+	void Start () {
+        Screen.orientation = ScreenOrientation.Portrait;
+        StartC = GameObject.Find("StartCanvas").GetComponent<Canvas>();
+        HUD = GameObject.Find("HUD").GetComponent<Canvas>();
+        End = GameObject.Find("EndCanvas").GetComponent<Canvas>();
+    }
+
+    public void TheEnd(int moves, bool finished, int pairs)
+    {
+        if (finished)
+        {
+            points = 30;
+            if(moves > 20)
+            {
+                points = 30 - (moves - 20);
+                if (points < 0)
+                {
+                    points = 0;
+                }
+            }
+
+            GameObject.Find("Points").GetComponent<Text>().text = "You made " + moves.ToString() + " moves\nYou scored " + points.ToString() + " MP";
+
+        }
+        else
+        {
+            points = pairs * 3 - 5;
+            if (moves > 20)
+            {
+                points = points - (moves - 20);
+            }
+            if (points < 0)
+            {
+                points = 0;
+            }
+
+            GameObject.Find("Points").GetComponent<Text>().text = "You opened " + pairs.ToString() + " pairs in " + moves.ToString() + " moves\nYou scored " + points.ToString() + " MP";
+        }
+        HUD.enabled = false;
+        End.enabled = true;
+    }
+
+    public void ClickStart()
+    {
+        GameObject.FindGameObjectWithTag("Tile").GetComponent<MuistiTile>().StartPressed();
+        StartC.enabled = false;
+        HUD.enabled = true;
+    }
+
+    public void ClickContinue()
+    {
+        int P = PlayerPrefs.GetInt("Points");
+        PlayerPrefs.SetInt("Points", P + PlayerPrefs.GetInt(CurrentGameHigh));
+        if (PlayerPrefs.GetInt("Playmode") == 1)
+        {
+            SceneManager.LoadScene("TourMenu", LoadSceneMode.Single);
+        }
+        else
+        {
+            SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+        }
+    }
+
+    public void ClickNewGame()
+    {
+        SceneManager.LoadScene("Muisti", LoadSceneMode.Single);
+    }
+
+    private IEnumerator NewGameStart()
+    {
+        yield return new WaitForSeconds(0.1f);
+        ClickStart();
+    }
+}
---
+++ FlagWorldGame/Assets/Games/Memory/Scripts/MuistiTile.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuistiTile : MonoBehaviour {
+
+    private static MuistiTile previousSelected = null;
+    private static bool Started = false;
+    private static bool CanClick = true;
+
+    private SpriteRenderer render, maretarium;
+    private Sprite CardBack, Picture;
+    private bool isSelected = false;
+    private Color32 back, front;
+    private MuistiManager MM;
+
+    [HideInInspector] public bool Paired = false;
+
+    void Start () {
+        MM = GameObject.Find("Manager").GetComponent<MuistiManager>();
+        render = GetComponent<SpriteRenderer>();
+        //SpriteRenderer[] Srs = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        //maretarium = Srs[1];
+        back = render.color;
+        front = new Color32(255, 255, 255, 255);
+        CardBack = render.sprite;
+	}
+
+    private void Select()
+    {
+        isSelected = true;
+        render.sprite = Picture;
+        render.color = front;
+        //maretarium.enabled = false;
+        previousSelected = gameObject.GetComponent<MuistiTile>();
+    }
+
+    private void Deselect()
+    {
+        isSelected = false;
+        render.sprite = CardBack;
+        render.color = back;
+        //maretarium.enabled = true;
+        previousSelected = null;
+    }
+
+    private void OnMouseDown()
+    {
+        if (Paired || !Started || !CanClick)
+        {
+            Debug.Log("Not Clickable");
+            return;
+        }
+
+        Debug.Log("Clickable");
+        if (isSelected)
+        { // 2 Is it already selected?
+            Deselect();
+            MM.GetMove();
+        }
+        else
+        {
+            if (previousSelected == null)
+            { // 3 Is it the first tile selected?
+                Select();
+            }
+            else
+            {
+                render.sprite = Picture;
+                render.color = front;
+                //maretarium.enabled = false;
+                CheckPair();
+            }
+        }
+    }
+
+    private void CheckPair()
+    {
+        if(Picture == previousSelected.GetPicture())
+        {
+            Paired = true;
+            previousSelected.Paired = true;
+            previousSelected = null;
+        }
+        else
+        {
+            StartCoroutine(WrongPair());
+        }
+        MM.GetMove();
+    }
+
+    private IEnumerator WrongPair()
+    {
+        CanClick = false;
+        yield return new WaitForSeconds(0.3f);
+        previousSelected.Deselect();
+        Deselect();
+        CanClick = true;
+    }
+
+    public void SetPicture(Sprite img)
+    {
+        Picture = img;
+    }
+
+    public Sprite GetPicture()
+    {
+        return Picture;
+    }
+
+    public void StartPressed()
+    {
+        if (!Started)
+        {
+            Started = true;
+        }
+        else
+        {
+            Started = false;
+        }
+    }
+}
