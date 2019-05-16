c71782d5e1b24259f01d89e35396a5c6221190d0
Teemu Turku <teemu.turku@pp2.inet.fi>
2019-05-16 09:53:44 +0300

    Viisari kelloon

--- FlagWorldGame/Assets/SharedAssets/Scripts/TimerClockScript.cs
+++ FlagWorldGame/Assets/SharedAssets/Scripts/TimerClockScript.cs
@@ -7,16 +7,24 @@ public class TimerClockScript : MonoBehaviour
 {
     // Put the radial image in this variable
     public Image radialImg;
+    // Rect transform of the timer hand
+    public RectTransform handRT;
     // Variable used to check if the clock should be updating
     bool isOn;
     float maxTime;
     float currentTime;
+    public float currentRot;
+    float maxRot;
 
     private void Start() 
     {
         if(!radialImg)
         {
             radialImg = transform.GetChild(1).GetComponent<Image>();
+        }
+        if(!handRT)
+        {
+            handRT = transform.GetChild(3).GetComponent<RectTransform>();
         }
     }
 
@@ -25,6 +33,8 @@ public class TimerClockScript : MonoBehaviour
         if(isOn)
         {
             radialImg.fillAmount = currentTime / maxTime;
+            currentRot = (currentTime / maxTime) * maxRot;
+            handRT.eulerAngles = new Vector3(0.0f, 0.0f, currentRot);
         }
     }
 
@@ -32,6 +42,8 @@ public class TimerClockScript : MonoBehaviour
     {
         maxTime = newMaxTime;
         currentTime = maxTime;
+        maxRot = 360f;
+        currentRot = maxRot;
         radialImg.fillAmount = currentTime / maxTime;
         isOn = true;
     }
