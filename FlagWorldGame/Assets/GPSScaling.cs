7ed9fd0a7e6faada1b9858f0e8f882c0f146fc62
Helistinify <Heikki.vayrynen92@gmail.com>
2019-11-22 12:28:53 +0200

    Some scaling for menus
    Credits and Help changed

---
+++ FlagWorldGame/Assets/GPSScaling.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSScaling : MonoBehaviour
+{
+    public Image HelpI, CreditsI;
+
+    private void Awake()
+    {
+        if (Screen.height / Screen.width <= 1.5f)
+        {
+            HelpI.GetComponent<RectTransform>().sizeDelta = new Vector2(800, 1150);
+            CreditsI.GetComponent<RectTransform>().sizeDelta = new Vector2(800, 1150);
+        }
+    }
+}
